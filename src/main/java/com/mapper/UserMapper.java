a9038d45c4c007a38d862774853e394ac7a6fe67
godd <goddenty@163com>
2017-07-04 10:18:51 +0800

    comment

--- src/main/java/com/mapper/UserMapper.java
+++ src/main/java/com/mapper/UserMapper.java
@@ -3,8 +3,13 @@ package com.mapper;
 import com.bean.User;
 
 public interface UserMapper {
-	
+	/**
+	 * 根据ID查询用户信息
+	 * @param id
+	 * @return
+	 */
 	public User getUserById(Integer id);
+	public User getUserByUsername(String username);
 	
 
 }
